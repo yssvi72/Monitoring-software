385861afd621750e013a70968c35b0355a470c5e
yssvi72 <141433982+yssvi72@users.noreply.github.com>
2023-08-05 20:34:29 +0530

    recordaudio.py

---
+++ recordaudio.py
@@ -0,0 +1,25 @@
+# Records audio of employee
+import wave, struct
+
+# load all audio input devices
+
+for index, device in enumerate(PvRecorder.get_audio_devices()):
+    print(f"[{index}] {device}")
+
+recorder = PvRecorder(device_index=-1, frame_length=512)  # (32 milliseconds of 16 kHz audio)
+audio = []
+path = 'audio_recording.wav'
+
+try:
+    recorder.start()
+
+    while True:
+        frame = recorder.read()
+        audio.extend(frame)
+except KeyboardInterrupt:
+    recorder.stop()
+    with wave.open(path, 'w') as f:
+        f.setparams((1, 2, 16000, 512, "NONE", "NONE"))
+        f.writeframes(struct.pack("h" * len(audio), *audio))
+finally:
+    recorder.delete()
